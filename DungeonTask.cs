4081e8ef4b125909394139d44322ee207585676b
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2024-01-31 11:08:44 +0300

    final

--- DungeonTask.cs
+++ DungeonTask.cs
@@ -17,55 +17,35 @@ public class DungeonTask
         _start = map.InitialPosition;
         _exit = map.Exit;
         _chests = map.Chests;
+
         var pathToExit = GetPathToExit();
-        if (pathToExit == null )
+        if (pathToExit == null)
             return new MoveDirection[0];
 
-        IEnumerable<SinglyLinkedList<Point>> pathOnStart = BfsTask.FindPaths(_map, _start, _chests);
-            
+        var pathOnStart = BfsTask.FindPaths(_map, _start, _chests).ToList();
+
         if (pathOnStart.Count() == 0 || _chests.Any(chest => pathToExit.Contains(chest)))
         {
             return GetMoveDirections(pathToExit);
         }
 
-        var pathOnChestToExit = BfsTask.FindPaths(_map, _exit, _chests);
+        var path = GetResultPath(pathOnStart);
 
-
-        //var path = GetMinPath(sumPaths);
-
-        //var paths = from exitToChest in pathOnStart
-        //            join startToChest in pathOnChestToExit
-        //            on exitToChest.Value equals startToChest.Value
-        //            select new { exitToChest = exitToChest, startToChest = startToChest, Length = exitToChest.Length + startToChest.Length };
-
-        //var path = pathOnStart
-        //    .Select(g => (  g,  pathOnChestToExit.Where(p => p.Value == g.Value).First() ));
-
-        var path2 = pathOnStart
-            .Join(pathOnChestToExit,
-                p => p.Value,
-                g => g.Value,
-                (p, g) => new { p, g, l = p.Length + g.Length });
-
-        //var p1 = path.Where(p => p.Count() > 0)
-        //    .OrderBy(p => p.Count())
-        //    .First();
-
-        return new MoveDirection[0];
-        //return GetMoveDirections(path);
+        return GetMoveDirections(path);
     }
 
-    static IEnumerable<Point> GetMinPath(IEnumerable<(SinglyLinkedList<Point> a, SinglyLinkedList<Point> b, int length)> paths)
+    private static IEnumerable<Point> GetResultPath(List<SinglyLinkedList<Point>> pathOnStart)
     {
-        var minLenth = int.MaxValue;
-        var minPath = paths.First();
-        foreach (var path in paths)
-        {
-            if ((path.Item1.Length + path.Item2.Length) < minLenth)
-                minPath = path;
-        }
-
-        return  minPath.Item1.Concat(minPath.Item2.Skip(1));
+        return pathOnStart
+                    .Select(p => BfsTask.FindPaths(_map, p.Value, new[] { _exit }).First())
+                    .Join(pathOnStart,
+                        p => p.Last(),
+                        g => g.Value,
+                        (p, g) => p.Concat(g.Skip(1)))
+                    .OrderBy(p => p.Count())
+                    .First()
+                    .Reverse()
+                    .ToList();
     }
 
     private static MoveDirection[] GetMoveDirections(IEnumerable<Point> path)
